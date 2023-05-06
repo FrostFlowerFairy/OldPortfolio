782ba6f6ebfcd3f013bb945854f0a6eded9d00aa
Wizardly3915 <wizardlydeveloper@gmail.com>
2023-05-07 05:22:37 +0800

    fix: change resume link

--- src/portfolio.js
+++ src/portfolio.js
@@ -16,7 +16,7 @@ const greeting = {
   subTitle1: "Full Stack Developer.",
   subTitle2: "Life Long Learner.",
   subTitle: "Full Stack Developer, Life Long Learner.",
-  resumeLink: "https://drive.google.com/file/d/17NxTsZHXbNiLiSuE6HRQ8dBoTrG-N2Sa/view?usp=share_link",
+  resumeLink: "https://drive.google.com/file/d/1iVHBC0U9BQFRZ27RnEUjmsNcG-Rg3Y0X/view?usp=share_link",
   mail: "mailto:bowenmanuel48@gmail.com@gmail.com",
 };
 
