fd7cf3c69af65a0023809d1aa43122a5cda70461
Wizardly3915 <wizardlydeveloper@gmail.com>
2023-05-19 18:48:28 +0000

    fix: Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -16,7 +16,7 @@ const greeting = {
   subTitle1: "Full Stack Web Developer.",
   subTitle2: "Life Long Learner.",
   subTitle: "Full Stack Web Developer, Life Long Learner.",
-  resumeLink: "https://drive.google.com/file/d/1iVHBC0U9BQFRZ27RnEUjmsNcG-Rg3Y0X/view?usp=share_link",
+  resumeLink: "https://drive.google.com/file/d/1KGuWYTWZVIEZSKue0UQA5KuaiWVLyWMC/view?usp=share_link",
   mail: "mailto:bowenmanuel48@gmail.com",
 };
 
