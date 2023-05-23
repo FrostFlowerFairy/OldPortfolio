fe53c40847dd4c3023f1c91c7402902bf649aae4
Wizardly3915 <wizardlydeveloper@gmail.com>
2023-05-23 20:38:08 +0000

    Update FeelingProud.js

--- src/containers/greeting/FeelingProud.js
+++ src/containers/greeting/FeelingProud.js
@@ -4,7 +4,7 @@ import "./FeelingProud.css";
 
 function FeelingProud(props) {
   // const theme = props.theme;
-  return <img style={{borderRadius: '50%', width: '400px', marginLeft: '100px'}} src={portfolioPhoto} alt="portfolio-profile" />;
+  return <img style={{borderRadius: '50%', maxWidth: '400px', margin: '0 100px'}} src={portfolioPhoto} alt="portfolio-profile" />;
 }
 
 export default FeelingProud;
